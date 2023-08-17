1c29eb2fdbf7b8f0313def209b75bfd9aa51af71
Mark Townsend <whackablemole@gmail.com>
2023-08-17 20:05:21 +0100

    Created new ViteJs prpject (React).

---
+++ vite-investigation-frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>This is just a Vite Investigation</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 24)}>
+          count is {count}
+        </button>
+        <p>
+          Seems pretty damn fast to me captain! Updates before I can even tab back to the page!
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
